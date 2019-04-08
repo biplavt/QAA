2e66768162b5b2ac842fe1d3a014246c3a4e6236
Biplav Timalsina <biplavt@Hawsco.com>
2019-04-07 22:09:29 -0700

    CORS enable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mysql=require('mysql');
-const bodyParser=require('body-parser');
+const bodyParser=require('body-parser');q
 var cors = require('cors');
 
 const PORT = process.env.PORT || 3060;
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 3060;
 app.use(bodyParser.json());
 
 
-app.use(require('./routes/allRoutes'),function(req,res,next){
-	next();
-});
+
 
 var originsWhitelist = [
     'http://localhost:4200' //this is my front-end url for development
@@ -36,6 +34,10 @@ app.use(function(req, res, next) {
     next();
 });
 
+
+app.use(require('./routes/allRoutes'),function(req,res,next){
+	next();
+});
 
 app.listen(PORT, function() {
     console.log(`Server started at ${PORT}...`);
